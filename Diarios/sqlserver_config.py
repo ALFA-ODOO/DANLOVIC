01354c27571b3136a052fea51cc43daa8b06f7f2
Alberto Antunez <albertoantunez@hotmail.com>
2025-07-24 20:59:11 -0300

    corrijo con odoo diario

--- Diarios/sqlserver_config.py
+++ Diarios/sqlserver_config.py
@@ -3,14 +3,6 @@
 #SQL Server Native Client 10.0
 #ODBC Driver 17 for SQL Server
 
-# sql_server = {
-#     "server": "SERVER-DB",
-#     "database": "ELTREBOL2021",
-#     "user": "ELTREBOL2021",
-#     "password": "ELTREBOL2021",
-#     "driver": "ODBC Driver 17 for SQL Server"
-# }
-
 sql_server = {
     "server": "WIN-TUNPH1OHJM9\ALFANET",
     "database": "DISTRIWALTERP",
