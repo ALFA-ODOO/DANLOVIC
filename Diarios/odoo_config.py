936604b5ce7c5aa8ad257d4c7a11bcca648f0f4b
Alberto Antunez <albertoantunez@hotmail.com>
2025-07-24 20:57:39 -0300

    corrijo conexion a odoo danlovic

--- Diarios/odoo_config.py
+++ Diarios/odoo_config.py
@@ -1,10 +1,5 @@
 # odoo_config.py
-# url = "https://eltrebol-pruebas.odoo.com"
-# db = "eltrebol-pruebas"
-# username = "albertoantunez@hotmail.com"
-# password = "Alfa02051987"
-
-url = "https://alfa-net-soluciones-informaticas1.odoo.com"
-db = "alfa-net-soluciones-informaticas1"
-username = "albertoantunez@hotmail.com"
-password = "Alfa02051987"
+url = "https://danlovic.odoo.com"
+db = "danlovic"
+username = "hernanwalter@gmail.com"
+password = "CRMdanlovic2025"
